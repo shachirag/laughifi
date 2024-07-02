eca7cd832e3f97b4937aef04e2a9f0f24330c80f
Chirag Sharma <chirag.sharma@teqexpert.com>
2024-07-02 14:01:16 +0530

    done

--- server.go
+++ server.go
@@ -75,58 +75,10 @@ func main() {
 		}
 	}))
 
+	http.Handle("/schema", srv)
+
 	http.Handle("/", playground.Handler("GraphQL Playground", "/query"))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL Playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
-// package main
-
-// import (
-// 	"laughifi/config"
-// 	"laughifi/database"
-// 	graph "laughifi/graph/resolvers"
-// 	"log"
-// 	"net/http"
-// 	"os"
-
-// 	"github.com/99designs/gqlgen/graphql/handler"
-// 	"github.com/99designs/gqlgen/graphql/playground"
-// )
-
-// const defaultPort = "8080"
-
-// func main() {
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = defaultPort
-// 	}
-
-// 	err := database.SetupAWSClient()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	err = config.LoadENV()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	db := database.Connect()
-// 	s3 := database.GetS3Uploader()
-// 	ses := database.GetSesClient()
-// 	resolver := &graph.Resolver{
-// 		DB:        db,
-// 		S3Client:  s3,
-// 		SESClient: ses,
-// 	}
-// 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
-
-// 	http.Handle("/query", srv)
-
-// 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-
-// 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-// 	log.Fatal(http.ListenAndServe(":"+port, nil))
-// }
