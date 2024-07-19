936aa859e4d2924464a1a1bff47d18decafd1345
Chirag Sharma <chirag.sharma@teqexpert.com>
2024-07-19 19:14:42 +0530

    done

--- handlers/admin/trivia/editTrivia.go
+++ handlers/admin/trivia/editTrivia.go
@@ -58,7 +58,7 @@ func EditTrivia(ctx context.Context, db *database.DB, triviaId string, input mod
 			"category.id":        categoryObjID,
 			"category.name":      category.Name,
 			"dareForWrongAnswer": dareForYouUrl,
-			"correctAnswer":      input.Answers,
+			"correctAnswer":      input.CorrectAnswer,
 			"updatedAt":          time.Now().UTC(),
 		},
 	}
